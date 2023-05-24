95031e424f82a957d6ecf057015b3d94fa2fd071
kjh970519 <115525671+kjh970519@users.noreply.github.com>
2023-05-24 22:55:34 +0900

    Add files via upload

--- res/js/app.js
+++ res/js/app.js
@@ -88,28 +88,28 @@
             if(a.remainingTime < b.remainingTime) return -1;
         });
 
-        openingIslandIdx = [...new Set(openingIslandIdx)];
-        _openingIslandIdx = [];
+        // openingIslandIdx = [...new Set(openingIslandIdx)];
+        // _openingIslandIdx = [];
 
-        let idx = 0;
-        nearTime.forEach(function(data) {            
-            for (var i=0; i < openingIslandIdx.length; i++) {
-                if (data.idx === openingIslandIdx[i]) _openingIslandIdx.push(idx);
-            }            
-            idx++;
-        });
+        // let idx = 0;
+        // nearTime.forEach(function(data) {            
+        //     for (var i=0; i < openingIslandIdx.length; i++) {
+        //         if (data.idx === openingIslandIdx[i]) _openingIslandIdx.push(idx);
+        //     }            
+        //     idx++;
+        // });
 
-        for (var i=0; i < _openingIslandIdx.length; i++) {
-            let tmp = nearTime[_openingIslandIdx[i]];
-            nearTime.splice(_openingIslandIdx[i], 1);
-            nearTime.unshift(tmp);
-        }
+        // for (var i=0; i < _openingIslandIdx.length; i++) {
+        //     let tmp = nearTime[_openingIslandIdx[i]];
+        //     nearTime.splice(_openingIslandIdx[i], 1);
+        //     nearTime.unshift(tmp);
+        // }
 
-        let _islandData = [];
-        for (var i=0; i < nearTime.length; i++) {
-            _islandData.push(islandData[nearTime[i].idx]);
-        }
-        islandData = _islandData;
+        // let _islandData = [];
+        // for (var i=0; i < nearTime.length; i++) {
+        //     _islandData.push(islandData[nearTime[i].idx]);
+        // }
+        // islandData = _islandData;
     }
 
     function createCard(islandName, img, remainingTime, reward, nextRemainingTime, isOpening) 
